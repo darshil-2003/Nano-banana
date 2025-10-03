a6ced68f95070da5b7f904efa70048e8de99511f
mac <darshil@infinitycorp.tech>
2025-10-03 14:27:55 +0530

    add

--- src/components/heroSection/Hero/playgroundContainer/playgroundOutput/loadingStates/index.tsx
+++ src/components/heroSection/Hero/playgroundContainer/playgroundOutput/loadingStates/index.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import Image from "next/image";
-import Loader from "@/components/loader";
+import Loader from "@/components/abc";
 
 interface LoadingStatesProps {
   generationState: {
